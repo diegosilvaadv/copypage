44b9624a130305aa96cbe9d3e4723b438f5e46cf
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-01 15:34:28 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/perfil/perfil_widget.dart
+++ lib/pages/perfil/perfil_widget.dart
@@ -960,6 +960,46 @@ class _PerfilWidgetState extends State<PerfilWidget>
                         ],
                       ),
                     ),
+                    Padding(
+                      padding:
+                          EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 0.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FFButtonWidget(
+                            onPressed: () async {
+                              GoRouter.of(context).prepareAuthEvent();
+                              await authManager.signOut();
+                              GoRouter.of(context).clearRedirectLocation();
+
+                              context.pushNamedAuth('login', context.mounted);
+                            },
+                            text: 'SAIR',
+                            options: FFButtonOptions(
+                              height: 40.0,
+                              padding: EdgeInsetsDirectional.fromSTEB(
+                                  24.0, 0.0, 24.0, 0.0),
+                              iconPadding: EdgeInsetsDirectional.fromSTEB(
+                                  0.0, 0.0, 0.0, 0.0),
+                              color: Color(0xFF88180F),
+                              textStyle: FlutterFlowTheme.of(context)
+                                  .titleSmall
+                                  .override(
+                                    fontFamily: 'Readex Pro',
+                                    color: Colors.white,
+                                  ),
+                              elevation: 3.0,
+                              borderSide: BorderSide(
+                                color: Colors.transparent,
+                                width: 1.0,
+                              ),
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                     if (columnUsersRow?.produtores ?? true)
                       Expanded(
                         child: Padding(
